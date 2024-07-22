76350f0bbf2932f7bd954604936ccbecfe952fd4
eyadcode24 <eyadcode24@gmail.com>
2024-07-22 11:41:00 +0200

    final 2

--- src/test/java/eyad/dev/models/CalculatorImcTest.java
+++ src/test/java/eyad/dev/models/CalculatorImcTest.java
@@ -7,10 +7,6 @@ import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import eyad.dev.models.CalculatorImc;
-import eyad.dev.models.Person;
-
 public class CalculatorImcTest {
     
     CalculatorImc calculator;
